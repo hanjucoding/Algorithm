4d1803a1049f49b343423093f9d73bcab46c11c0
hanjucoding <142489569+hanjucoding@users.noreply.github.com>
2024-07-23 09:15:07 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 128 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -1,24 +1,20 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
+
 
 public class Main {
-	public static long factorial(long n) {
-		if(n == 0 ) {
-			return 1;
-		}
-		else {
-			return factorial(n-1)*n;
-		}
-		
-		
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		long N = sc.nextInt();
-		
-		System.out.println(factorial(N));
-		
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Long.parseLong(br.readLine());
+        System.out.println(factorial(N));
+        }
 
-}
+
+        static long factorial(long n) {
+            if(n == 0) return 1;
+            else return n * factorial(n-1);
+        }
+    }
+
+
