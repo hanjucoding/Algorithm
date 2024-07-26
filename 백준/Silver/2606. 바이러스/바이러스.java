67a17d7767b16a49e6056c81a01aad4530e9c3a7
hanjucoding <142489569+hanjucoding@users.noreply.github.com>
2024-07-26 12:30:32 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,35 +2,47 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int result = 0;
-    static int node,branch;
     static int[][] arr;
     static boolean[] visited;
+    static int N, T, result;
+    static Queue<Integer> queue;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        node = Integer.parseInt(br.readLine());
-        branch = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        T = Integer.parseInt(br.readLine());
 
-        arr = new int[node+1][node+1];
-        visited = new boolean[node+1];
-        for (int i = 0; i < branch; i++) {
+        arr = new int[N + 1][N + 1];
+        visited = new boolean[N + 1];
+
+        for (int i = 0; i < T; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             arr[x][y] = 1;
             arr[y][x] = 1;
         }
-        dfs(1);
-        System.out.println(result-1);
+        result = 0;
+        queue = new LinkedList<>();
+        bfs(1);
+        System.out.println(result);
+
     }
-    static void dfs(int V) {
-        visited[V] = true;
-        result++;
-        for (int i = 0; i < node+1; i++) {
-            if (arr[V][i] == 1 && !visited[i]) {
-                dfs(i);
+
+    static void bfs(int n) {
+        visited[n] = true;
+        queue.add(n);
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            for (int i = 1; i < N+1; i++) {
+                if(arr[node][i] == 1 && !visited[i]) {
+                    visited[i] = true;
+                    queue.add(i);
+                    result++;
+                }
             }
         }
     }
 }
+
