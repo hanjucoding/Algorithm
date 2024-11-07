a36311eb3b787c738d72d0a061939546e586a34d
전한주 <142489569+hanjucoding@users.noreply.github.com>
2024-11-07 20:54:07 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 208 ms, Memory: 24940 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -0,0 +1,59 @@
+import java.io.*;
+
+public class Main {
+		static char arr[][];
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
+		arr = new char[N][N];
+
+		makeStar(0, 0, N, false);
+
+		for(int i=0; i<N; i++) {
+			bw.write(arr[i]);
+			bw.write('\n');
+		}
+
+		bw.flush();
+		bw.close();
+
+	}
+
+	static void makeStar(int x, int y, int N, boolean blank) {
+
+		if(blank) {
+			for(int i=x; i<x + N; i++) {
+				for(int j=y; j<y + N; j++) {
+					arr[i][j] = ' ';
+				}
+			}
+			return;
+		}
+
+		if(N == 1) {
+			arr[x][y] = '*';
+			return;
+		}
+
+		int size = N / 3;
+		int count = 0;
+		for(int i=x; i<x + N; i+=size) {
+			for(int j=y; j<y + N; j+=size) {
+				count ++;
+
+				if(count == 5) {
+					makeStar(i, j, size, true);
+				}
+				else {
+					makeStar(i, j, size, false);
+				}
+			}
+		}
+		
+
+    }
+}
